119cd5c0da24e3ac280ae86193069fa255cd68f6
Vishal Kunchal <91224087+Vishalkun@users.noreply.github.com>
2023-10-20 11:50:34 +0530

    Update PowerOf2.java

--- src/day5program/PowerOf2.java
+++ src/day5program/PowerOf2.java
@@ -3,21 +3,25 @@ package day5program;
 public class PowerOf2 {
 
 	public static void main(String[] args) {
-		if (args.length != 1) {
-            System.out.println("Please provide one integer command-line argument");
-            return;
+        int n, pow, i;
+        for(i = 0; i < 10; i++)
+        {
+            pow = 1;
+            n = i;
+
+            while(n > 0)
+            {
+                pow *= 2;
+                n--;
+            }
+            System.out.print(pow);
+
         }
+    }
 
-        int N = Integer.parseInt(args[0]);
-
-        if (N < 0 || N >= 31) {
-            System.out.println("N must be in the range ");
-            return;
-        }
-
-        for (int i = 0; i <= N; i++) {
-            int powerOf2 = (int) Math.pow(2, i);
-            System.out.println("2^" + i + " = " + powerOf2);
+	public static void main(String[] args) {
+		PowerOf2  p= new PowerOf2();
+		p.power2();
+		
 	}
-
 }}
