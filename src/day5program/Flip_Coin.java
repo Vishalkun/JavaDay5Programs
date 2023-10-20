b2922f81fc6a021298b73922898507825ea40409
Vishal Kunchal <91224087+Vishalkun@users.noreply.github.com>
2023-10-20 11:37:45 +0530

    Update Flip_Coin.java

--- src/day5program/Flip_Coin.java
+++ src/day5program/Flip_Coin.java
@@ -10,31 +10,41 @@ public class Flip_Coin {
 		Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the number of times to flip the coin: ");
-        int numberOfFlips = input.nextInt();
+        int flip = input.nextInt();
         input.close();
 
-        if (numberOfFlips <= 0) {
-            System.out.println("Please enter a positive integer for the number of flips.");
-            return;
-        }
+        int head = 0;
+        int tail= 0;
+        int headcount = 0;
+        int tailcount = 0;
 
-        int headsCount = 0;
-        int tailsCount = 0;
-        Random random = new Random();
+        double headpercentage;
+        double tailpercentage;
 
-        for (int i = 0; i < numberOfFlips; i++) {
-            if (random.nextDouble() < 0.5) {
-                tailsCount++;
-            } else {
-                headsCount++;
+        
+            for(int i = 0;i<flip;i++)
+            {
+                Random random = new Random();
+                int coin = random.nextInt(2);
+
+              if(coin<0.5)
+              {
+            	  System.out.println("tails");
+                tailcount++;
+				
+              }
+            else
+              {
+            	System.out.print("head");
+            	headcount++;
+              }
             }
-        }
+        headpercentage = (headcount * 100) / flip;
+        tailpercentage= (tailcount * 100)/flip;
+        System.out.print("No of headpercentage" + headpercentage);
+        System.out.print("No of tailpercentage" + tailpercentage);
 
-        double headsPercentage =  headsCount / numberOfFlips * 100;
-        double tailsPercentage =  tailsCount / numberOfFlips * 100;
 
-        System.out.println("Heads: " + headsCount + " (" + headsPercentage + "%)");
-        System.out.println("Tails: " + tailsCount + " (" + tailsPercentage + "%)");
     }
 	}
 
