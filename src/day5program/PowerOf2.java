42211f1487633408e084273a1480e06bc813984a
Vishal Kunchal <91224087+Vishalkun@users.noreply.github.com>
2023-10-20 14:46:43 +0530

    Update PowerOf2.java

--- src/day5program/PowerOf2.java
+++ src/day5program/PowerOf2.java
@@ -2,8 +2,8 @@ package day5program;
 
 public class PowerOf2 {
 	public static void power2()
-    {
-        int n, pow, i;
+	{
+		int n, pow, i;
         for(i = 0; i < 10; i++)
         {
             pow = 1;
@@ -20,42 +20,10 @@ public class PowerOf2 {
     }
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 7a37e48b598ef8df572291857b50a060dca85069
-        int n, pow, i;
-        for(i = 0; i < 10; i++)
-        {
-            pow = 1;
-            n = i;
 
-            while(n > 0)
-            {
-                pow *= 2;
-                n--;
-            }
-            System.out.print(pow);
-
-        }
-    }
-
-	public static void main(String[] args) {
-<<<<<<< HEAD
->>>>>>> 7a37e48b598ef8df572291857b50a060dca85069
-=======
->>>>>>> 7a37e48b598ef8df572291857b50a060dca85069
 		PowerOf2  p= new PowerOf2();
 		p.power2();
 		
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
+	}
 	
-}
-=======
-=======
->>>>>>> 7a37e48b598ef8df572291857b50a060dca85069
-}}
->>>>>>> 7a37e48b598ef8df572291857b50a060dca85069
