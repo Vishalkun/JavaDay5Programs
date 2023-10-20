2513bf623628e7f541338475e1aa291ecadae2f7
Vishal Kunchal <91224087+Vishalkun@users.noreply.github.com>
2023-10-20 11:45:16 +0530

    Update LeapYear.java

--- src/day5program/LeapYear.java
+++ src/day5program/LeapYear.java
@@ -7,27 +7,19 @@ public class LeapYear {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter a year (4 digits): ");
+        System.out.print("Enter a year");
         int year = input.nextInt();
         input.close();
 
-        if (year >= 1000 && year <= 9999) {
-            if (isLeapYear(year)) {
-                System.out.println(year + " is a leap year.");
-            } else {
-                System.out.println(year + " is not a leap year.");
-            }
-        } else {
-            System.out.println("Please enter a valid 4-digit year.");
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+        {
+            System.out.print("it is a Leap Year."+ year);
         }
-    }
-
-    public static boolean isLeapYear(int year) {
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            return true;
-        } else {
-            return false;
-        }
+        else
+        {
+            System.out.print("it is not a Leap Year:"+ year);
+              
 	}
 	}
+}
 
