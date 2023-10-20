3a68644261d311b7f7c9bf6a97ac7dc9fa89f974
Vishal Kunchal <91224087+Vishalkun@users.noreply.github.com>
2023-10-20 14:55:45 +0530

    Update Prime_Factors.java

--- src/day5program/Prime_Factors.java
+++ src/day5program/Prime_Factors.java
@@ -1,43 +1,21 @@
 package day5program;
 public class Prime_Factors {
-	 public static void primeFactor(int n) {
-		    System.out.print("Prime Factors : ");
-		    for (int i = 2; i * i <= n || n > 1; i++) {
+	    System.out.print("Prime Factors : ");
+	    for (int i = 2; n > 1; i++) {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	public static void main(String[] args) System.out.print("Prime Factors : ");
-		    for (int i = 2; i * i <= n || n > 1; i++) {
+	      if (n % i == 0) {
 
->>>>>>> 7a37e48b598ef8df572291857b50a060dca85069
-=======
-	public static void main(String[] args) System.out.print("Prime Factors : ");
-		    for (int i = 2; i * i <= n || n > 1; i++) {
+	        while (n % i == 0) {
 
->>>>>>> 7a37e48b598ef8df572291857b50a060dca85069
-		      if (n % i == 0) {
+	          System.out.print(i + " ");
+	          n = n / i;
+	        }
+	      }
 
-		        while (n % i == 0) {
-
-		          System.out.print(i + " ");
-		          n = n / i;
-		        }
-		      }
-
-		    }
-		  }
+	    }
+	  }
+	
 	public static void main(String[] args) {
 		 int n = 60;
 		    primeFactor(n);
-<<<<<<< HEAD
-<<<<<<< HEAD
-}}
-=======
-=======
->>>>>>> 7a37e48b598ef8df572291857b50a060dca85069
-        }
-	}
-
-}
->>>>>>> 7a37e48b598ef8df572291857b50a060dca85069
+	}}
